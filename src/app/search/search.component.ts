c6d241ea82de041f7f41df1f82830aebc6e32186
apex39 <mateuszbak3@gmail.com>
2017-08-21 17:04:25 +0200

    Ad autocompletion

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from "@angular/forms";
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
 
 @Component({
   selector: 'app-search',
@@ -7,10 +10,58 @@ import {FormControl} from "@angular/forms";
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
   queryCrtl: FormControl;
-  filteredQueries: any;
-  states: String[];
+  filteredQueries: Observable<string[]>;
+  states = ['Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming'];
   searchOptions = [
     'Release',
     'Artist',
@@ -19,61 +70,14 @@ export class SearchComponent implements OnInit {
 
   constructor() {
     this.queryCrtl = new FormControl();
+    this.filteredQueries = this.queryCrtl.valueChanges.startWith(null).map(value => this.filterQueries(value));
   }
 
   ngOnInit() {
-    this.states = [
-      'Alabama',
-      'Alaska',
-      'Arizona',
-      'Arkansas',
-      'California',
-      'Colorado',
-      'Connecticut',
-      'Delaware',
-      'Florida',
-      'Georgia',
-      'Hawaii',
-      'Idaho',
-      'Illinois',
-      'Indiana',
-      'Iowa',
-      'Kansas',
-      'Kentucky',
-      'Louisiana',
-      'Maine',
-      'Maryland',
-      'Massachusetts',
-      'Michigan',
-      'Minnesota',
-      'Mississippi',
-      'Missouri',
-      'Montana',
-      'Nebraska',
-      'Nevada',
-      'New Hampshire',
-      'New Jersey',
-      'New Mexico',
-      'New York',
-      'North Carolina',
-      'North Dakota',
-      'Ohio',
-      'Oklahoma',
-      'Oregon',
-      'Pennsylvania',
-      'Rhode Island',
-      'South Carolina',
-      'South Dakota',
-      'Tennessee',
-      'Texas',
-      'Utah',
-      'Vermont',
-      'Virginia',
-      'Washington',
-      'West Virginia',
-      'Wisconsin',
-      'Wyoming',
-    ];
   }
 
+  filterQueries(value: string) {
+    return value ? this.states.filter(query => query.toLowerCase().indexOf(value.toLowerCase()) === 0)
+      : this.states;
+  }
 }
