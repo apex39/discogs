08495660a6ed3ad4b133d0aa3341727eb14f6fbc
Jacek Adamczyk <adamczyk.jac@gmail.com>
2017-09-11 15:19:18 +0100

    First set of unit tests. Corrected indentation.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from "@angular/http";
+import { MdAutocompleteModule, MdInputModule, MdRadioModule } from '@angular/material';
+import { SearchComponent } from './search/search.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SearchComponent
+      ],
+      imports: [
+        HttpModule,
+        BrowserAnimationsModule,
+        MdRadioModule,
+        MdInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MdAutocompleteModule
       ],
     }).compileComponents();
   }));
+
+  const appTitle = "Disoggs"
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -17,16 +34,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Disoggs'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual(appTitle);
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain(appTitle);
   }));
 });
