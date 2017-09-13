2e365c01fdd97a65492c688f5ae87d270f7a4110
Jacek Adamczyk <adamczyk.jac@gmail.com>
2017-09-13 13:55:19 +0100

    Imports in search Component suite, checking API endpoint in SearchService tests.

--- src/app/search.service.spec.ts
+++ src/app/search.service.spec.ts
@@ -1,15 +1,33 @@
+import { ReflectiveInjector } from '@angular/core'
 import { TestBed, inject } from '@angular/core/testing';
+import { BaseRequestOptions, ConnectionBackend, Http, RequestOptions } from '@angular/http';
+import { Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [SearchService]
-    });
+    this.injector = ReflectiveInjector.resolveAndCreate([
+      {provide: ConnectionBackend, useClass: MockBackend},
+      {provide: RequestOptions, useClass: BaseRequestOptions},
+      Http,
+      SearchService,
+    ]);
+    this.searchService = this.injector.get(SearchService);
+    this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+    this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
   });
 
-  it('should be created', inject([SearchService], (service: SearchService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('search() should query current service url', () => {
+    // GIVEN
+    let searchOption = 'option';
+    let query = 'query'
+    let expectedUrl = 'api.discogs.com/database/search'
+    // WHEN
+    this.searchService.search(query, searchOption);
+    // THEN
+    expect(this.lastConnection).toBeDefined('no http service connection at all?');
+    expect(this.lastConnection.request.url).toMatch(expectedUrl, 'url invalid');
+  });
 });
