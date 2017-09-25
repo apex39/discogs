ff56282c98d7d802c85594df57bec3da8d1fc36b
mateusz <mateuszbak3@gmail.com>
2017-09-25 10:05:32 +0200

    Bind result to album details component

--- src/app/album-details/album-details.component.ts
+++ src/app/album-details/album-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Result} from '../search/model/members/result';
 
 @Component({
   selector: 'app-album-details',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-details.component.css']
 })
 export class AlbumDetailsComponent implements OnInit {
-
+@Input() result: Result;
   constructor() { }
 
   ngOnInit() {
